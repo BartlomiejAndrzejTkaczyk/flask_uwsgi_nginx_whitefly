f1dec9441875055d0919b7a2c5b5b47dd579eb12
kruk97 <76618368+BartlomiejAndrzejTkaczyk@users.noreply.github.com>
2024-05-29 21:30:45 +0200

    add loader io

---
+++ loader_io/router.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, jsonify
+
+router = Blueprint('loader_io', __name__, url_prefix='')
+
+
+@router.route('/loaderio-4f2d0dc9a3c15e9dbb0a87e4de154b4a/', methods=['GET'])
+def loader_io():
+    return jsonify({'loader_io': 'loaderio-4f2d0dc9a3c15e9dbb0a87e4de154b4a'})
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 from user.router import router as user_router
 from user_async.router import router as user_async_router
+from loader_io.router import router as loader_io_router
 
 app = Flask(__name__)
 app.register_blueprint(user_router)
 app.register_blueprint(user_async_router)
+app.register_blueprint(loader_io_router)
 
 
 @app.route('/')
